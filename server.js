7f9c5fbd6b4da2cd89d2b14da91ad1af524f6ce7
HrS GOD <95730735+Botsailer@users.noreply.github.com>
2023-08-05 23:50:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const WebSocket = require('ws');
+const fs = require('fs');
 
 const wss = new WebSocket.Server({ port: 8080 });
 
@@ -7,11 +8,38 @@ wss.on('connection', (ws) => {
 
   ws.on('message', (message) => {
     console.log(`Received message: ${message}`);
+    try {
+      const data = JSON.parse(message);
+      if (data.type === 'file') {
+        saveFileData(data.fileData);
+      } else if (data.type === 'text') {
+        handleTextMessage(data.text);
+      } else {
+        console.log('Unknown message type:', data.type);
+      }
+    } catch (error) {
+      console.error('Error parsing message:', error);
+    }
   });
-setTimeout(() => {
-  ws.send("call_log");},5000);
+  setTimeout(() => {
+    ws.send(JSON.stringify({ type: 'text', text: 'call_log' }));
+  }, 5000);
 
   ws.on('close', () => {
     console.log('Client disconnected');
   });
 });
+function saveFileData(fileData) {
+  const filePath = './uploads/' + Date.now() + '.txt';
+  fs.writeFile(filePath, fileData, (err) => {
+    if (err) {
+      console.error('Error saving file:', err);
+    } else {
+      console.log('File saved:', filePath);
+    }
+  });
+}
+
+function handleTextMessage(text) {
+  console.log('Received text message:', text);
+}
